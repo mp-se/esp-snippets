4f70f1b1ff81f20f6eaa9d094d45907257b4b4a1
Magnus <mp-se@users.noreply.github.com>
2025-01-26 10:23:34 +0100

    Cleanup code

--- snippets/voltage/main-voltage.cpp
+++ snippets/voltage/main-voltage.cpp
@@ -1,25 +1,24 @@
 /*
-  MIT License
+MIT License
 
-  Copyright (c) 2025 Magnus Persson
+Copyright (c) 2025
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-  SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
  */
 #include <log.hpp>
 
@@ -58,7 +57,7 @@ class Config {
 #elif defined(ESP32C3)
     return 1.70;  // 220k + 220k
 #elif defined(ESP32S2)
-    return 1.65; 
+    return 1.65; // 220k + 220k
 #elif defined(ESP32S3)
     return 2.02;  // 220k + 220k
 #elif defined(ESP32)
@@ -85,7 +84,6 @@ BatteryVoltage::BatteryVoltage() {
 #if defined(ESP8266)
   pinMode(myConfig.getVoltagePin(), INPUT);
 #else
-  // pinMode(myConfig.getVoltagePin(), INPUT_PULLDOWN);
   pinMode(myConfig.getVoltagePin(), INPUT);
   analogReadResolution(SOC_ADC_MAX_BITWIDTH);
   analogSetAttenuation(ADC_11db); // Max possible input is 2.7V
